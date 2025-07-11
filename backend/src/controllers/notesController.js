d3f923368d7bc833038a0da62bc009d0e94bf713
Erik <104572224+Erik-Sandqvist@users.noreply.github.com>
2025-07-11 21:43:44 +0200

    create, get, update

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,5 +1,13 @@
+import Note from '../models/Note.js';
+
 export async function getAllNotes(req, res) {
-    res.status(200).send("Here are your notes");
+   try {
+        const notes = await Note.find()
+        res.status(200).json(notes);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error retrieving notes", error: error.message });
+    }
 }
 
 export async function getNoteById(req, res) {
@@ -8,12 +16,36 @@ export async function getNoteById(req, res) {
 }
 
 export async function createNote(req, res) {    
-    const newNote = req.body;
-    res.status(201).json({ message: "Note created successfully"});
+    try {
+      const { title, content } = req.body;
+      const note = new Note({
+        title,
+        content,
+      });
+        const savedNote = await note.save();
+        res.status(201).json(savedNote);
+      console.log(title, content);
+      
+    } catch (error) {
+       
+    }
 }
 
 export async function updateNote(req, res) {
-    res.status(200).json({ message: `Note updated successfully`});
+    try {
+        const { title, content } = req.body;
+
+      const updatedNote = await Note.findByIdAndUpdate(req.params.id, { title, content },
+       { new: true, }
+      );
+        if (!updatedNote) {
+            return res.status(404).json({ message: "Note not found" });
+        }
+        res.status(200).json(updatedNote);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error updating note", error: error.message });
+    }
 }
 
 export async function deleteNote(req, res) {
