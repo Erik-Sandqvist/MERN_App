d789dee395c524eee5023e3c6851a0610e3409b8
Erik <104572224+Erik-Sandqvist@users.noreply.github.com>
2025-07-22 14:03:34 +0200

    ratelimit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from "./config/db.js";
 // const express = require("express");
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config(); 
 
@@ -14,6 +15,12 @@ const port = process.env.PORT || 5001;
 connectDB();
 
 app.use(express.json()); 
+app.use(rateLimiter)
+
+app.use((req,res,next) => {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+}); // Middleware to log requests
 
 app.use("/api/notes", notesRoutes); 
 
